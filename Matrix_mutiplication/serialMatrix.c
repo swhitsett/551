e88745fd6f11c94e56874402470833acf8b23742
swhitsett <kongzero32@gmail.com>
2015-04-08 10:32:06 -0700

    can now enter matrices

--- Matrix_mutiplication/serialMatrix.c
+++ Matrix_mutiplication/serialMatrix.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void reciveMatrix(int,int);
+void reciveMatrix(int n,int matrix[]);
 
-void reciveMatrix(int n, int matrix ){
+void reciveMatrix(int n, int matrix[] ){
 	int row;
 	int colum;
 	for(row=0; row<n; row++){
 		for(colum=0; colum<n; colum++){
-			scanf("%i", matrix[row*n+colum]);
+			scanf("%i", &matrix[row*n+colum]);
 		}
 	}
 }
 
 int main(){
 
+	char flag;
+	char form[3];
 	int *matrix1;
 	int *matrix2;
-	char form[3];
-	char flag;
 	int n;
 
 	scanf("%s %c %d",form, &flag, &n);
@@ -26,8 +26,17 @@ int main(){
 	matrix1 = malloc(n*n*sizeof(int));
 	matrix2 = malloc(n*n*sizeof(int));
 
-	reciveMatrix(n, &matrix1);
-	reciveMatrix(n, &matrix2);
+	reciveMatrix(n, matrix1);
+	reciveMatrix(n, matrix2);
+	
+	// for printing pourpses --------------
+	// int i;
+	// int j;
+	// for (i = 0; i < n; i++) {
+	//      for (j = 0; j < n; j++)
+	//         printf("%d ", matrix1[i*n+j]);
+	//      printf("\n");
+	//   	}
 
 	free(matrix1);
 	free(matrix2);
