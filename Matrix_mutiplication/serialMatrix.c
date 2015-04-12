205b599c1c6ca183df28616cdb3e6aa4f5dd7ec2
swhitsett <kongzero32@gmail.com>
2015-04-11 19:50:22 -0700

    ijk serial working

--- Matrix_mutiplication/serialMatrix.c
+++ Matrix_mutiplication/serialMatrix.c
@@ -4,6 +4,7 @@
 
 void reciveMatrix(int n,int matrix[]);
 void multiMatrixIJK(int n, int matrixA[], int matrixB[], int reslutC[]);
+void printMatrix(int n, int matrix[]);
 
 void reciveMatrix(int n, int matrix[] ){
 	int row;
@@ -18,14 +19,26 @@ void reciveMatrix(int n, int matrix[] ){
 void multiMatrixIJK(int n, int matrixA[], int matrixB[], int reslutC[]){
 	
 }
-
+void printMatrix(int n, int matrix[]){
+	//for printing pourpses --------------
+	int i;
+	int j;
+	for (i = 0; i < n; i++) {
+     for (j = 0; j < n; j++)
+        printf("%d ", matrix[i*n+j]);
+     printf("\n");
+  	}
+	// for(i=0; i<n; i++)
+	// 	printf("%d\n",matrix[i] );
+}
 int main(){
 
 	char flag;
 	char form[3];
-	int *matrix1;
-	int *matrix2;
-	int *reslutMatrix;
+	int *matrix1 = NULL;
+	int *matrix2 = NULL;
+	int *reslutMatrix = NULL;
+	int *tempVector = NULL;
 	int n;
 
 	scanf("%s %c %d",form, &flag, &n);
@@ -33,6 +46,7 @@ int main(){
 	matrix1 = malloc(n*n*sizeof(int));
 	matrix2 = malloc(n*n*sizeof(int));
 	reslutMatrix = malloc(n*n*sizeof(int));
+	tempVector = malloc(n*sizeof(int));
 
 	reciveMatrix(n, matrix1);
 	reciveMatrix(n, matrix2);
@@ -41,30 +55,27 @@ int main(){
 		int i,j,k;
 		for(i=0; i<n; i++){
 			for(j=0; j<n; j++){
+				tempVector[i] = 0;
 				for(k=0; k<n; k++){
-					reslutMatrix[]
+
+					tempVector[i] += matrix1[i*n+k] * matrix2[k*n+j];
 				}
+				reslutMatrix[i*n+j] = tempVector[i];
+				// printf("%i\n",tempVector[i] );
 			}
 		}
 	}
-	else if(strcmp("ikj",form) == 0)){
-		// printf("needs work");// multiMatrixIKJ();
+	else if(strcmp("ikj",form) == 0){
+		printf("needs work");// multiMatrixIKJ();
 	}
-	else if(strcmp("kij",form) == 0)){
+	else if(strcmp("kij",form) == 0){
 		printf("needs work2");// multiMatrixKIJ();
 	}
 	else
 		printf("Please specifiy a correct Form");
 
-	//for printing pourpses --------------
-	// int i;
-	// int j;
-	// for (i = 0; i < n; i++) {
-	//      for (j = 0; j < n; j++)
-	//         printf("%d ", matrix2[i*n+j]);
-	//      printf("\n");
-	//   	}
-
+	printMatrix(n, reslutMatrix);
+	
 	free(matrix1);
 	free(matrix2);
 	free(reslutMatrix);
