fa4260dca2edcb42ddefb35e4e6e7c5fc302f8fd
swhitsett <kongzero32@gmail.com>
2015-05-10 23:57:54 -0700

    Old version taking new method

--- Gelimination/gElim.c
+++ Gelimination/gElim.c
@@ -1,37 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main(){
-
+#include <string.h>
+#include <math.h>
+// #include <omp.h>
+void upperTriangular(double** A, int n, int threads){
+	
 	int i;
 	int j;
 	int k;
-	int n;
-	double sum = 0.0;
 	double C;
-	double **A = NULL;
-	double *X;
-	// double C;
-
-	scanf("%d\n", &n);
-
-	A = malloc(n * sizeof(double));
-	X = malloc(n * sizeof(double));
-	// C = malloc(n * n * sizeof(double));
-
-	// populateMatrix();
-	for(i=0; i<n; i++){
-		A[i] = malloc((n+1)* sizeof(double));
-
-		for(j=0; j<(n+1); j++){
-			// printf("A[%d][%d]: ", i,j);
-			scanf("%lf",&A[i][j]);
-		}
-	}
 
 	for(j=0; j<n; j++){
 		for(i=0; i<n; i++){
-			if(i>j){
+			if(i>j){										//selection of pivot
 				C=A[i][j]/A[j][j];
 				for(k=0; k<n+1; k++){
 					A[i][k]=A[i][k]-C*A[j][k];
@@ -39,102 +20,89 @@ int main(){
 			}
 		}
 	}
+}
 
-	X[n-1]=A[n-1][n]/A[n-1][n-1];
-
+void backwardSub(double** A, double* X, int n, int threads){
+	int i;
+	int j;
+	double sum;
 	/* this loop is for backward substitution*/
-	for(i=n-2; i>=0; i--)
+	for(i=n-1; i>=0; i--)
 	{
 		sum=0.0;
 		for(j=i+1; j<=n-1; j++)
 		{
 			// printf("%lf and %lf\n", A[i][j], X[j]);
 			sum=sum+A[i][j]*X[j];
-			// printf("A[%d][%d] * x[%d]\n",i,j,j );
+			// printf("sum= %lf A[%d][%d]= %lf X[%d]= %lf\n", sum, i, j, A[i][j], j, X[j]);
 		}
 		X[i]=(A[i][n]-sum)/A[i][i];
 		// printf("x[%d]=(A[%d][%d]-%lf)/A[%d][%d]\n",i,i,n,sum,i,i );
 	}
-
-	for(i=0; i<n; i++)
-	{
-		printf("\nx%d=%f\t",i,X[i]); /* x1, x2, x3 are the required solutions*/
-	}
-	return 0;
 }
 
-// #include <stdio.h>
-// #include <stdlib.h>
+void populateMatrix(double** A, int n){
+	int i;
+	int j;
 
-// int main(){
+	for(i=0; i<n; i++){
+		A[i] = malloc((n+1)* sizeof(double));
 
-// 	int i;
-// 	int j;
-// 	int k;
-// 	int n;
-// 	double C;
-// 	double **A = NULL;
-// 	double *X;
-// 	// double C;
+		for(j=0; j<(n+1); j++){
+			// if(j = (n+1))
+			// 	C[i] = drand48();
+			// else
+			// 	A[i][j] = drand48();
 
-// 	scanf("%d\n", &n);
+			scanf("%lf",&A[i][j]);
+		}
+	}
+}
 
-// 	A = malloc(n * sizeof(double));
-// 	X = malloc(n * sizeof(double));
-// 	// C = malloc(n * n * sizeof(double));
+int main(int argc, char *argv[]){
 
-// 	// populateMatrix();
-// 	for(i=0; i<n; i++){
-// 		A[i] = malloc((n+1)* sizeof(double));
+	int i;
+	int j;
+	int k;
+	int n;
+	int threads;
+	// double sum = 0.0;
+	// double C;
+	double **A = NULL;
+	double *X;
+	// double C;
 
-// 		for(j=0; j<(n+1); j++){
-// 			printf("A[%d][%d]: ", i,j);
-// 			scanf("%lf",&A[i][j]);
-// 		}
-// 	}
-	
-// 	printf("\n\n");
-// 	for (i = 0; i < n; i++) {
-//         for (j = 0; j < n+1; j++)
-//             printf("%lf ", A[i][j]);
-//         printf("\n");
-//     }
+	if(argc == 3){
+		n = atoi(argv[1]);
+		threads = atoi(argv[2]);
+	}
 
-// 	//calculate matrix
-// 	for(j=0; j<n; j++){
-// 		for(i=0; i<n; i++){
-// 			if(i >= j){
-// 				C = A[i][j] / A[j][j];
-// 				for(k=0; k<(n); k++){
-// 					A[i][k] = A[i][k] - C * A[j][k];
-// 				}
-// 			}
-// 		}
-// 	}
-	
-// 	X[n-1] = A[n-1][n] / A[n-1][n-1];
-// 	for(i = n - 1; i >= 0; i--)
-//     {
-//         X[i] = A[i][n] / A[i][i];
-//         for(j = i - 1; j >= 0; j--)
-//         {
-//             A[j][n] = A[j][i] * X[i];
-//         }
-//     }
-// 	// for(i=n-1; i>0; i--){
-// 	// 	int sum=0;
-// 	// 	for(j=i+1; j<n; j++){
-// 	// 		sum=sum+A[i][j]*X[j];
-// 	// 	}
-// 	// 	X[i]=(A[i][(n+1)]-sum)/A[i][i];
-// 	// }
+	scanf("%d\n", &n);
 
-	
+	A = malloc(n * sizeof(double));
+	X = malloc(n * sizeof(double));
+	// C = malloc(n * n * sizeof(double));
 
-// 	for(i=0; i<n; i++)
-// 	{
-// 		printf("\nx%d=%lf\t",i,X[i]); /* x1, x2, x3 are the required solutions*/
-// 	}
+	populateMatrix(A, n);
 
-// 	return 0;
-// }
+	// for(i=0; i<n; i++){
+	// 	A[i] = malloc((n+1)* sizeof(double));
+
+	// 	for(j=0; j<(n+1); j++){
+	// 		// printf("A[%d][%d]: ", i,j);
+	// 		scanf("%lf",&A[i][j]);
+	// 	}
+	// }
+
+	upperTriangular(A, n, threads);
+
+	// X[n-1]=A[n-1][n]/A[n-1][n-1];
+	backwardSub(A, X, n, threads);
+
+	for(i=0; i<n; i++)
+		printf("\nx%d=%f\t",i,X[i]);
+
+	free(A);
+	free(X);
+	return 0;
+}
