3a7c2c767e445361679f0bdeb083c766202b16e8
swhitsett <kongzero32@gmail.com>
2015-05-15 10:27:58 -0700

    clean up

--- Gelimination/gElim.c
+++ Gelimination/gElim.c
@@ -1,15 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include <math.h>
 #include <omp.h>
-#include <time.h>
 
 int threads;
-int thread_count;
+int threads_used;
 
 void createMatrix(double** A, double* B, 
-                  double** originalMatrix,
-                  double* originalVector, int n){
+                  double** initMatrix,
+                  double* initVec, int n){
     int i;
     int j;
     double tmp;
@@ -17,16 +17,16 @@ void createMatrix(double** A, double* B,
 
     for(i=0; i<n; i++){
         A[i] = malloc(n * sizeof(double));
-        originalMatrix[i] = malloc(n * sizeof(double));
+        initMatrix[i] = malloc(n * sizeof(double));
 
         for(j=0; j<(n+1); j++){
             tmp = drand48() * 2e6 - 1e6;
             A[i][j] = tmp;
-            originalMatrix[i][j] = tmp;
+            initMatrix[i][j] = tmp;
         }
         tmp = drand48() * 2e6 - 1e6;
         B[i] = tmp;
-        originalVector[i] = tmp;
+        initVec[i] = tmp;
     }
 }
 
@@ -34,15 +34,17 @@ void upperTriangular(double **A, double * B, int n){
     int i;
     int j;
     int k;
+    int greatest;
     double tmp;
     double tmp2;
     double maxVal;
     
     for(i = 0; i < n-1;i++) {
-        int greatest = i;
+        
+        greatest = i;
 
-        // #pragma omp parallel for num_threads(threads) default(none) \
-         43                 shared(n, A, B, i, greatest) private(j)
+        #pragma omp parallel for num_threads(threads) default(none) \
+                       shared(n, A, B, i, greatest) private(j)
         for(j = i; j < n; j++) {
             if(fabs(A[j][i]) > fabs(maxVal)){
                 maxVal = A[j][i];
@@ -50,8 +52,7 @@ void upperTriangular(double **A, double * B, int n){
             }
         }
 
-        for(k = i; k < n; k++)
-        {
+        for(k = i; k < n; k++){
             tmp = A[greatest][k];
             A[greatest][k] = A[i][k];
             A[i][k] = tmp;
@@ -72,38 +73,36 @@ void upperTriangular(double **A, double * B, int n){
     }
 }
 
-void backSubstitute(double** A, double* B, double* C, int n){
+void backSub(double** A, double* B, double* C, int n){
 
     int i;
     int j;
     for(i = n-1; i >= 0; i--) {
         C[i] = B[i] / A[i][i];
         #pragma omp parallel for num_threads(threads) default(none) \
-                shared(A, B, n, i, C, thread_count) private(j)
+                shared(A, B, n, i, C, threads_used) private(j)
         for(j = n-1; j >= 0; j--) {
             B[j] -= A[j][i] * C[i];
-            if(i == 0 && j == 0) 
-                thread_count = omp_get_num_threads();
+            if(j == 0)             
+                threads_used = omp_get_num_threads();
         }
     }
 }
 
-void computeResidual(double** originalMatrix, double* originalVector,
+void computeResidual(double** initMatrix, double* initVec,
                      double* C, double* X, int n){
 
     int i;
     int j;
-    // double* result = malloc(sizeof(double) * n);
     #pragma omp parallel for num_threads(threads) default(none) \
-        shared(originalMatrix, originalVector, C, n, X) private(i, j)
+        shared(initMatrix, initVec, C, n, X) private(i, j)
     for(i = 0; i < n; i++) {
         X[i] = 0.0;
         for(j = 0; j < n; j++) {
-            X[i] += originalMatrix[i][j] * C[j];
+            X[i] += initMatrix[i][j] * C[j];
         }
-        X[i] = originalVector[i] - X[i];
+        X[i] = initVec[i] - X[i];
     }
-    // return result;
 }
 
 double l2norm(double* X, int n){
@@ -123,13 +122,16 @@ int main(int argc, char* argv[]){
     // int i;
     // int j;
     double start, finished, elapsed;
+    double** initMatrix;
+    double* initVec;
     double** A;
     double* B;
     double* C;
     double* X;
+    double L2;
     
     if(argc == 3) {
-        n       = atoi(argv[1]);
+        n = atoi(argv[1]);
         threads = atoi(argv[2]);
     }
     else{
@@ -137,28 +139,30 @@ int main(int argc, char* argv[]){
         return 0;
     }
     
-    double** originalMatrix = malloc(n * sizeof(double*));
-    A              = malloc(n * sizeof(double*));
-    double* originalVector  = malloc(n * sizeof(double));
-    B               = malloc(n * sizeof(double));
-    C               = malloc(n * sizeof(double));
-    X               = malloc(n * sizeof(double));
+    initMatrix = malloc(n * sizeof(double*));
+    initVec = malloc(n * sizeof(double));
+    A = malloc(n * sizeof(double*));
+    B = malloc(n * sizeof(double));
+    C = malloc(n * sizeof(double));
+    X = malloc(n * sizeof(double));
 
 
-    createMatrix(A, B, originalMatrix, originalVector, n);
+    createMatrix(A, B, initMatrix, initVec, n);
     
     start = omp_get_wtime();
 
     upperTriangular(A, B, n);
-    backSubstitute(A,B,C,n);
-    computeResidual(originalMatrix, originalVector, C, X, n);
+    backSub(A, B, C, n);
+    computeResidual(initMatrix, initVec, C, X, n);
 
     finished = omp_get_wtime();
     elapsed = finished - start;
 
-    printf("Procs used:        %d\n", omp_get_num_procs());
-    printf("Threads used:      %d\n", thread_count);
-    printf("Elapsed time:      %f\n", elapsed);
-    printf("Residual l^2 norm: %f\n", l2norm(X, n));
+    // l2norm(X, L2, n);
+
+    printf("Processors used: %d\n", omp_get_num_procs());
+    printf("Threads used: %d\n", threads_used);
+    printf("Elapsed time: %f\n", elapsed);
+    printf("L2 norm: %f\n", l2norm(X, n));
     return 0;
 }
