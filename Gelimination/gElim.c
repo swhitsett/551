186f2e1528f2da874389158ad6a702f922299c70
swhitsett <kongzero32@gmail.com>
2015-05-16 16:45:23 -0700

    go test

--- Gelimination/gElim.c
+++ Gelimination/gElim.c
@@ -1,12 +1,41 @@
+/*
+
+ * CSCI Assingnment 4 Gaussian Elimination
+ *
+ *
+ *  To compile: gcc -fopenmp -lm -Wall -o gElim gElim.c
+ *
+ *  To run: ./gElim matrix_size desired_threads
+ *
+ *  Input:
+ *      matrix_size : desired size of the n x n matrix
+ *      desired_threads : desired threads to be used in parallized sections
+ *
+ *  Output will consist of: the amount of processors, the amout of threads used,
+ *      over time of execution and the l^2 norm.
+ *
+ */
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 #include <math.h>
 #include <omp.h>
 
-int threads;
-int threads_used;
+int threads;            //Global Varible for Threads specified by user
+int threads_used;       //Global Varible Threads actually used by program
 
+/*-------------------------------------------------------------------*/
+/*  Function: createMatrix
+    
+    Variables:
+        A: The desired matrix n x n
+        B: The result vector n
+        initMatrix: A 2nd copy of the Matrix n x n (same values)
+        n: The desired size of the matrix
+
+    Purpose: Function will loop through the rows and colums to create a 
+             matrix by randomly generated input using drand48()     
+  -------------------------------------------------------------------*/
 void createMatrix(double** A, double* B, 
                   double** initMatrix,
                   double* initVec, int n){
@@ -29,8 +58,23 @@ void createMatrix(double** A, double* B,
         initVec[i] = tmp;
     }
 }
+/*-------------------------------------------------------------------*/
+/*  Function: upperTriangular
+    
+    Variables:
+        A: Populated n x n matrix
+        B: Populated vector of n size
+        n: max index size
 
-void upperTriangular(double **A, double * B, int n){
+    Purpose: contains the following fuctions of Gaussian Elimination
+             partial pivioting - relocation of rows with larger values
+                                 before the matrix is converted to 
+                                 upper triangular form.
+             upper Triangular form  - Conversion of the n x n matrix
+                                      to upper triangular form using
+                                      annilation.
+  -------------------------------------------------------------------*/
+void upperTriangular(double **A, double* B, int n){
     int i;
     int j;
     int k;
@@ -39,20 +83,20 @@ void upperTriangular(double **A, double * B, int n){
     double tmp2;
     double maxVal;
     
-    for(i = 0; i < n-1;i++) {
+    for(i = 0; i < n-1;i++){
         
         greatest = i;
 
         #pragma omp parallel for num_threads(threads) default(none) \
                        shared(n, A, B, i, greatest) private(maxVal, j)
-        for(j = i; j < n; j++) {
+        for(j=i; j<n; j++){
             if(fabs(A[j][i]) > fabs(maxVal)){
                 maxVal = A[j][i];
                 greatest = j;
             }
         }
 
-        for(k = i; k < n; k++){
+        for(k=i; k<n; k++){
             tmp = A[greatest][k];
             A[greatest][k] = A[i][k];
             A[i][k] = tmp;
@@ -63,60 +107,101 @@ void upperTriangular(double **A, double * B, int n){
 
         #pragma omp parallel for num_threads(threads) default(none) \
                 shared(A, B, i, n) private(j, k)
-        for(j = i+1; j < n; j++) {
-            double multiplier = A[j][i] / A[i][i];
-            for(k = i; k < n; k++) {
-                A[j][k] -= multiplier * A[i][k];
+        for(j=(i+1); j<n; j++) 
+            tmp = A[j][i] / A[i][i];
+            for(k=i; k<n; k++){
+                A[j][k] -= tmp * A[i][k];
             }
-            B[j] -= multiplier * B[i];
+            B[j] -= tmp * B[i];
         }
     }
 }
+/*-------------------------------------------------------------------*/
+/*  Function: backSub
+    
+    Variables:
+        A: Populated n x n matrix in upper triangular form
+        B: Populated vector of n size
+        C: Blank matrix to hold values calculated of B[]/A[][]
+        n: max index size
 
+    Purpose: Performs bac subistution on the matrix A which is
+             currently in upper triangular form.
+  -------------------------------------------------------------------*/
 void backSub(double** A, double* B, double* C, int n){
 
     int i;
     int j;
-    for(i = n-1; i >= 0; i--) {
+    for(i=(n-1); i>=0; i--){
         C[i] = B[i] / A[i][i];
         #pragma omp parallel for num_threads(threads) default(none) \
                 shared(A, B, n, i, C, threads_used) private(j)
-        for(j = n-1; j >= 0; j--) {
+        for(j=(n-1); j>=0; j--){
             B[j] -= A[j][i] * C[i];
             if(j == 0)             
                 threads_used = omp_get_num_threads();
         }
     }
 }
+/*-------------------------------------------------------------------*/
+/*  Function: calcResidual
+    
+    Variables:
+        initMatrix: untouched n x n matrix 
+        initVec: untouched solution array
+        C: values calculated from backwards substitution B[]/A[][]
+        X: Blank array to hold the values of each rows residual value
+        n: index
 
-void computeResidual(double** initMatrix, double* initVec,
+    Purpose: Calculates the residual, showing the diffrence from the 
+             original values to the approxmate
+  -------------------------------------------------------------------*/
+void calcResidual(double** initMatrix, double* initVec,
                      double* C, double* X, int n){
 
     int i;
     int j;
     #pragma omp parallel for num_threads(threads) default(none) \
         shared(initMatrix, initVec, C, n, X) private(i, j)
-    for(i = 0; i < n; i++) {
+    for(i=0; i<n; i++){
         X[i] = 0.0;
-        for(j = 0; j < n; j++) {
+        for(j=0; j<n; j++){
             X[i] += initMatrix[i][j] * C[j];
         }
         X[i] = initVec[i] - X[i];
     }
 }
+/*-------------------------------------------------------------------*/
+/*  Function: l2norm
+    
+    Variables:
+            X: array of size n holding the residual values
+            L2: result of the calculated L2 Norm
+            n: index
+        
 
+    Purpose: Calculates the L^2 norm
+  -------------------------------------------------------------------*/
 void l2norm(double* X, double* L2, int n){
     
     double result = 0.0;
     int i;
     #pragma omp parallel for num_threads(threads) default(none) \
         shared(result, X, n) private(i)
-    for(i = 0; i < n; i++) {
+    for(i=0; i<n; i++){
         result += pow(X[i], 2);
     }
     *L2 = sqrt(result);
 }
+/*-------------------------------------------------------------------*/
+/*  The Main function will............
+    -Declare Varibles for processing
+    -Recive input for the the matrix size and desired threads
+    -Allocate memory for all Matrices and varibles
+    -Call the above functions to complete a Gaussian Equation.
+    -Take the complete time and display it to the user
 
+  -------------------------------------------------------------------*/
 int main(int argc, char* argv[]){
     int n;
     // int i;
@@ -130,17 +215,20 @@ int main(int argc, char* argv[]){
     double* X;
     double* L2;
     
-    n = 8000;
-    #pragma omp parallel	
-    threads = omp_get_num_threads(); 
-  //  if(argc == 3) {
-  //      n = atoi(argv[1]);
-  //      threads = atoi(argv[2]);
-  //  }
-  //  else{
-  //      printf("invalid input");
-  //      return 0;
-  //  }
+    // ---- Used for MTL values were hard coded -----
+    // n = 8000;
+    // #pragma omp parallel	
+    // threads = omp_get_num_threads(); 
+    //-----------------------------------------------
+
+    if(argc == 3) {
+       n = atoi(argv[1]);
+       threads = atoi(argv[2]);
+    }
+    else{
+       printf("invalid input");
+       return 0;
+    }
     
     initMatrix = malloc(n * sizeof(double*));
     initVec = malloc(n * sizeof(double));
@@ -161,7 +249,7 @@ int main(int argc, char* argv[]){
     finished = omp_get_wtime();
     elapsed = finished - start;
 
-    computeResidual(initMatrix, initVec, C, X, n);
+    calcResidual(initMatrix, initVec, C, X, n);
     l2norm(X, L2, n);
 
     printf("Processors used: %d\n", omp_get_num_procs());
